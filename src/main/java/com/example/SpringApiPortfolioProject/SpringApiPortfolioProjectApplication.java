d138d556b1805989e9ef0b2d4c2ecdcfde613f6e
donnyhyon <dhyon333@hotmail.co.uk>
2023-12-31 16:16:03 +0000

    initial commit

---
+++ src/main/java/com/example/SpringApiPortfolioProject/SpringApiPortfolioProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringApiPortfolioProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiPortfolioProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiPortfolioProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringApiPortfolioProject/SpringApiPortfolioProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringApiPortfolioProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiPortfolioProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
