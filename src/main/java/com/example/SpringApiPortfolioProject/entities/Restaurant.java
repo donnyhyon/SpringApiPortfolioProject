e083643e8e3cffa044fe6b7eeb896674dbf83576
donnyhyon <dhyon333@hotmail.co.uk>
2023-12-31 16:53:22 +0000

    created restaurant entity

--- src/main/java/com/example/SpringApiPortfolioProject/entities/Restaurant.java
+++ src/main/java/com/example/SpringApiPortfolioProject/entities/Restaurant.java
@@ -1,14 +1,11 @@
 package com.example.SpringApiPortfolioProject.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.Table;
 
 
 @Entity
