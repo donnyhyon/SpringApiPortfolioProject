b3ee2af8e1cb6b2b6cc003cf293887d21bc9cc90
donnyhyon <dhyon333@hotmail.co.uk>
2023-12-31 18:10:10 +0000

    fixed error in crud extension

--- src/main/java/com/example/SpringApiPortfolioProject/repositories/RestaurantRepository.java
+++ src/main/java/com/example/SpringApiPortfolioProject/repositories/RestaurantRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface RestaurantRepository extends CrudRepository {
+public interface RestaurantRepository extends CrudRepository<Restaurant,Long> {
     Optional<Restaurant> findRestaurantsByNameAndZipCode(String name, String zipCode);
     List<Restaurant> findRestaurantsByZipCodeAndPeanutScoreNotNullOrderByPeanutScore(String zipCode);
 
