1ccb058fbdc55a4a36439fce6ed3f2836c7149d1
donnyhyon <dhyon333@hotmail.co.uk>
2023-12-31 17:23:50 +0000

    created and extended restaurant repo

--- src/main/java/com/example/SpringApiPortfolioProject/entities/Restaurant.java
+++ src/main/java/com/example/SpringApiPortfolioProject/entities/Restaurant.java
@@ -1,6 +1,5 @@
 package com.example.SpringApiPortfolioProject.entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -16,6 +15,13 @@ public class Restaurant {
     @GeneratedValue
     private Long id;
 
-    @Column(name = "NAME")
     private String name;
+    private String street;
+    private String state;
+    private String zipCode;
+
+    private Integer overallScore;
+    private Integer peanutScore;
+    private Integer diaryScore;
+    private Integer eggScore;
 }
---
+++ src/main/java/com/example/SpringApiPortfolioProject/repositories/RestaurantRepoistory.java
@@ -0,0 +1,16 @@
+package com.example.SpringApiPortfolioProject.repositories;
+
+import com.example.SpringApiPortfolioProject.entities.Restaurant;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RestaurantRepoistory extends CrudRepository {
+    Optional<Restaurant> findRestaurantsByNameAndZipCode(String name, String zipCode);
+    List<Restaurant> findRestaurantsByZipCodeAndPeanutScoreNotNullOrderByPeanutScore(String zipCode);
+
+    List<Restaurant> findRestaurantsByZipCodeAndDiaryScoreNotNullOrderByDiaryScore(String zipCode);
+    List<Restaurant> findRestaurantsByZipCodeAndEggScoreNotNullOrderByEggScore(String zipCode);
+
+}
