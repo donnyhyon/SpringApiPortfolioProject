9f5fed58b656ed0ad4807bed3e7fa9dbfb0aef0f
donnyhyon <dhyon333@hotmail.co.uk>
2023-12-31 17:02:00 +0000

    created user enetity

--- src/main/java/com/example/SpringApiPortfolioProject/entities/User.java
+++ src/main/java/com/example/SpringApiPortfolioProject/entities/User.java
@@ -1,21 +1,26 @@
 package com.example.SpringApiPortfolioProject.entities;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Entity
 @Getter
 @Setter
-public class Restaurant {
+public class User {
     @Id
     @GeneratedValue
     private Long id;
 
-    @Column(name = "NAME")
     private String name;
+    private String city;
+    private String state;
+    private String zipcode;
+    private Boolean peanutAllergy;
+    private Boolean eggAllergy;
+    private Boolean diaryAllergy;
+
 }
