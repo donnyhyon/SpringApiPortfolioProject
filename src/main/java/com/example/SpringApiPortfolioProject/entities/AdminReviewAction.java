67967f6ee1516cfc74d8ae599e4a478941f2c462
donnyhyon <dhyon333@hotmail.co.uk>
2023-12-31 18:09:41 +0000

    Removed entity from admin review entity

--- src/main/java/com/example/SpringApiPortfolioProject/entities/AdminReviewAction.java
+++ src/main/java/com/example/SpringApiPortfolioProject/entities/AdminReviewAction.java
@@ -1,10 +1,8 @@
 package com.example.SpringApiPortfolioProject.entities;
 
-import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
 public class AdminReviewAction {
